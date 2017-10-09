646be30fbb984210f34495f50fa8d392a24e70e7
Marelle Hoevelmanns <marcelle.hoevelmanns@gmail.com>
2017-10-09 12:21:55 +0200

    modify form urls

--- woocommerce-affilicon-payment-gateway.php
+++ woocommerce-affilicon-payment-gateway.php
@@ -18,9 +18,9 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-define('AFFILICON_CHECKOUT_FORM_URL_LEGACY', 'https://secure-q.affilibank.de');
+define('AFFILICON_CHECKOUT_FORM_URL_LEGACY', 'https://secure.affilibank.de');
 define('AFFILICON_CHECKOUT_FORM_URL', 'https://secure.affilicon.net');
-define('AFFILICON_SERVICE_URL', 'https://service-q.affilicon.net/api');
+define('AFFILICON_SERVICE_URL', 'https://service.affilicon.net/api');
 
 
 $active_plugins = apply_filters('active_plugins', get_option('active_plugins'));
