e7facb9ede606899a6133bba336292144531ca30
Marcelle Hövelmanns <marcelle.hoevelmanns@gmail.com>
2018-07-27 15:36:43 +0200

    rename grid classes

--- templates/payment_fields.php
+++ templates/payment_fields.php
@@ -2,42 +2,42 @@
     #payment_method_affilicon_payment,
     .payment_box.payment_method_affilicon_payment,
     .payment_box.payment_method_affilicon_payment fieldset,
-    .container {
+    .af-container {
         background-color: #fff;
         padding: 10px 0;
         color: #777;
         font-family: "Helvetica Neue", Helvetica, "Segoe UI", Arial, sans-serif;
     }
-    .row {
+    .af-row {
         margin: -15px;
         padding: 10px 0;
         width: 100%;
         float: left;
     }
     
-    [class*="col-"], .col {
+    [class*="af-col-"], .af-col {
         padding: 15px;
         float:left;
         display: inline-block;
     }
     @media only screen and (min-width: 768px) {
         /* For desktop: */
-        .col-1 {width: 8.33%;}
-        .col-2 {width: 16.66%;}
-        .col-3 {width: 25%;}
-        .col-4 {width: 33.33%;}
-        .col-5 {width: 41.66%;}
-        .col-6 {width: 50%;}
-        .col-7 {width: 58.33%;}
-        .col-8 {width: 66.66%;}
-        .col-9 {width: 75%;}
-        .col-10 {width: 83.33%;}
-        .col-11 {width: 91.66%;}
-        .col-12 {width: 100%;}
+        .af-col-1 {width: 8.33%;}
+        .af-col-2 {width: 16.66%;}
+        .af-col-3 {width: 25%;}
+        .af-col-4 {width: 33.33%;}
+        .af-col-5 {width: 41.66%;}
+        .af-col-6 {width: 50%;}
+        .af-col-7 {width: 58.33%;}
+        .af-col-8 {width: 66.66%;}
+        .af-col-9 {width: 75%;}
+        .af-col-10 {width: 83.33%;}
+        .af-col-11 {width: 91.66%;}
+        .af-col-12 {width: 100%;}
     }
 
     @media only screen and (max-width: 768px) {
-        [class*="col-"], .col {
+        [class*="af-col-"], .af-col {
             width: 100%;
         }
         .d-md-block {
@@ -96,9 +96,9 @@
 </style>
 <fieldset>
     <div class="form-row form-row-wide">
-        <div class="container">
-            <div class="row">
-                <div class="col-4">
+        <div class="af-container">
+            <div class="af-row">
+                <div class="af-col-4">
                     <div class="logo-slogan">
                         <img src="<?= plugin_dir_url( __FILE__ ) ?>../assets/img/logo_checkout.png" alt="">
                         <div class="slogan text-uppercase">
@@ -106,7 +106,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-8 no-padding">
+                <div class="af-col-8 no-padding">
 
                     <div class="accepted-payment-methods text-uppercase">
                         <div class="text-center d-md-block">
@@ -121,16 +121,16 @@
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-4"></div>
-                <div class="col-8">
+            <div class="af-row">
+                <div class="af-col-4"></div>
+                <div class="af-col-8">
                     <div class="d-md-block text-uppercase text-center coupon-code">
                         <?php esc_html_e('You have a discount code? Add it in the next step.', 'woocommerce-affilicon-payment-gateway') ?>
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-12">
+            <div class="af-row">
+                <div class="af-col-12">
                     <div class="more-information">
                         <a href="<?php esc_html_e('url-informations-about-affilicon', 'woocommerce-affilicon-payment-gateway') ?>" target="_blank">
                             <?php esc_html_e('Further information about AffiliCon GmbH', 'woocommerce-affilicon-payment-gateway') ?>
